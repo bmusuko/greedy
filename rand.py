e15454997f079cb0085f16ab38a95e26f65e8e6a
bmusuko <32943475+bmusuko@users.noreply.github.com>
2019-02-08 22:03:17 +0700

    Add files via upload

--- rand.py
+++ rand.py
@@ -4,12 +4,14 @@ rand =[]
 for i in range (52):
 	rand.append(i)
 
-n1 = choice(rand)
-rand.remove(n1)
-n2 = choice(rand)
-rand.remove(n2)
-n3 = choice(rand)
-rand.remove(n3)
-n4 = choice(rand)
-print(n1," ",n2," ",n3," ",n4)
+for i in range(13):	
+	n1 = choice(rand)
+	rand.remove(n1)
+	n2 = choice(rand)
+	rand.remove(n2)
+	n3 = choice(rand)
+	rand.remove(n3)
+	n4 = choice(rand)
+	rand.remove(n4)
+	print(n1," ",n2," ",n3," ",n4)
 
